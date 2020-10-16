6224fe82733953460e2e3568ea09985152bb64de
Tore Berven <tore.berven@student.uib.no>
2020-10-16 15:28:28 +0200

    Added all files to git

---
+++ src/main/java/NewTask.java
@@ -0,0 +1,25 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class NewTask {
+
+    private final static String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+            String message = String.join(" ", args);
+            channel.basicPublish("", TASK_QUEUE_NAME, null, message.getBytes());
+            System.out.println("[x] sent '" + message + "'");
+        }
+    }
+
+
+}
---
+++ src/main/java/Worker.java
@@ -0,0 +1,43 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Worker {
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] recived '" + message + "'");
+
+            try {
+                doWork(message);
+            } finally {
+                System.out.println(" [x] done");
+            }
+        };
+        boolean autoAck = true;
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
+    }
+
+    private static void doWork(String task){
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
